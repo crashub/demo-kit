9c5b8a1e9bbe6e60ac8c6bf8ed35c6be17036e43
Alain Defrance <defrancea@gmail.com>
2013-09-11 05:54:19 -0700

    Fix monitor command

--- packaging/src/main/resources/monitor.groovy
+++ packaging/src/main/resources/monitor.groovy
@@ -9,7 +9,7 @@ def table = new UIBuilder().table(columns: [1], rows: [1,1,1]) {
       }
       row {
         eval {
-          execute("jmx find -p Catalina:j2eeType=Servlet,WebModule=*jmx-webapp,* | jmx get -a WebModule -a name -a servletClass -a requestCount -a processingTime | sort -f processingTime:desc -f WebModule -f name")
+          execute("jmx find -p Catalina:j2eeType=Servlet,WebModule=*jmx-webapp,* | jmx get WebModule name servletClass requestCount processingTime | sort -f processingTime:desc -f WebModule -f name")
         }
         table(columns: [1,2]) {
           row() {
@@ -24,14 +24,14 @@ def table = new UIBuilder().table(columns: [1], rows: [1,1,1]) {
               execute("jvm nonheap")
             }
           }
-          jvm.pools { name ->
+          (jvm.pools | { name ->
             row() {
               label("$name:")
               eval {
                 execute("jvm pool '$name'")
               }
             }
-          }
+          })()
         }
       }
     }
@@ -45,13 +45,13 @@ def table = new UIBuilder().table(columns: [1], rows: [1,1,1]) {
       }
       row {
         eval {
-          execute("jmx find -p Catalina:j2eeType=WebModule,* | jmx get -a name -a displayName -a stateName -a processingTime | sort -f name")
+          execute("jmx find -p Catalina:j2eeType=WebModule,* | jmx get name displayName stateName processingTime | sort -f name")
         }
         eval {
-          execute("jmx find -p Catalina:type=Manager,* | jmx get -a context -a sessionCounter -a sessionCreateRate | sort -f context")
+          execute("jmx find -p Catalina:type=Manager,* | jmx get context sessionCounter sessionCreateRate | sort -f context")
         }
         eval {
-          execute("jmx find -p Catalina:type=ThreadPool,name=\\\"http-bio-8080\\\",* | jmx get -a maxThreads -a currentThreadCount -a currentThreadsBusy");
+          execute("jmx find -p Catalina:type=ThreadPool,name=\\\"http-bio-8080\\\",* | jmx get maxThreads currentThreadCount currentThreadsBusy");
         }
       }
     }
